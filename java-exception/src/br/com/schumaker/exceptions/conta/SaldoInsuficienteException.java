c3c5e98610cb081341a6d3e79e51ccb55040ecb3
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-04-06 00:46:33 +0200

    Update SaldoInsuficienteException.java

--- java-exception/src/br/com/schumaker/exceptions/conta/SaldoInsuficienteException.java
+++ java-exception/src/br/com/schumaker/exceptions/conta/SaldoInsuficienteException.java
@@ -1,6 +1,6 @@
 package br.com.schumaker.exceptions.conta;
 
-public class SaldoInsuficienteException extends Exception{ //checked
+public class SaldoInsuficienteException extends Exception { //checked
 	
 	public SaldoInsuficienteException(String msg) {
 		super(msg);
